d0be14bc3d285b4844863b3dfcf75f9db0a04240
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2024-09-11 23:23:44 +0300

    Added clients and services; issues fixed

--- service/services/models.py
+++ service/services/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from clients.models import Client
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.IntegerField()
+
+    def __str__(self):
+        return f"Service: {self.name}"
+
+
+class Tariff(models.Model):
+    name = models.CharField(max_length=50)
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount')
+    )
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=20)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
+
+    def __str__(self):
+        return f"Tariff: {self.name}"
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name="subscription", on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name="subscription", on_delete=models.PROTECT)
+    plan = models.ForeignKey(Tariff, related_name="subscription", on_delete=models.PROTECT)
