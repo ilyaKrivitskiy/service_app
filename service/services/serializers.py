313ea9ae2db02784f787bec23b398254c8777f5a
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2024-09-19 16:13:05 +0300

    added feature get_price; issues fixed

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -13,7 +13,11 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     tariff = TariffSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.EmailField(source='client.user.email')
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return instance.service.full_price * (1 - instance.tariff.discount_percent / 100)
 
     class Meta:
         model = Subscription
-        fields = ('id', 'name', 'tariff_id', 'client_name', 'email', 'tariff')
+        fields = ('id', 'name', 'tariff_id', 'client_name', 'email', 'tariff', 'price')
